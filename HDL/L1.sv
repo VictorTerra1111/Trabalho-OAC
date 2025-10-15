08d82f5873f85f9a33d0b3268f4ae2320b89dfdd
Bernardo F. Fraga <bfoquesdefraga@gmail.com>
2025-10-15 21:33:49 +0000

    imodelando l1

--- HDL/L1.sv
+++ HDL/L1.sv
@@ -1,10 +1,20 @@
 module L1(
-    input logic end,
-    output logic end,
+    input logic end_cpu,
+    output logic end_mp,
 
-    input logic ctrl,
-    output logic ctrl,
+    input logic ctrl_cpu,
+    output logic ctrl_mp,
 
-    input logic status,
-    output logic status
-)
+    input logic status_cpu,
+    output logic status_mp,
+
+    inout logic data_cpu,
+    inout logic data_mp 
+);
+
+always @(posedge clock) begin
+    
+
+end
+
+endmodule
